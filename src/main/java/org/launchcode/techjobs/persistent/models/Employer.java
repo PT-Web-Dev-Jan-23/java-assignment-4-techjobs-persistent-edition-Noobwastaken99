84d548bc954189c682a8d5ca35c2a7a5d7b0ac6d
Noobwastaken99 <czmoni99@yahoo.com>
2023-05-28 11:50:31 -0500

    finished part two besides SQL code, failing Model in index for controllers though, started part 3 add List<job>

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -3,6 +3,8 @@ package org.launchcode.techjobs.persistent.models;
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
@@ -10,6 +12,9 @@ public class Employer extends AbstractEntity {
     @NotBlank(message = "Name is required")
     @Size(min = 3, max = 100, message = "Name must be between 3 and 100 characters long")
     private String location;
+    @OneToMany
+    @JoinColumn
+    private List<Job> jobs = new ArrayList<>();
 
     public Employer(String location) {
         this.location = location;
